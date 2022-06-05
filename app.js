247c46d69c707dfbfd628e2760fdb368de43f4fa
henry <enriqueafrutos@gmail.com>
2022-06-05 15:13:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+    let calculo = (prompt('quiere entradas 2d o 3d?' ))
+switch(calculo){
+    case '3d':
+        let entradas3d = parseInt(prompt('ingrese cantidad entradas 3d'))
+        document.write('El precio total cine 3d es $'+(entradas3d * 550))
+        break;
+        case '2d':
+            let entradas2d = parseInt(prompt('ingrese cantidad entradas 2d'))
+            document.write('el precio total cine 2d es $'+(entradas2d * 330))
+            break;
+            default:
+                document.write('no se puede')
+}
