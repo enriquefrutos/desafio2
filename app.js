0e7e943d2867ef5648f7c029a042f38553659bcf
henry <enriqueafrutos@gmail.com>
2022-06-05 15:17:31 -0300

    agrego precio

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
     let calculo = (prompt('quiere entradas 2d o 3d?' ))
 switch(calculo){
     case '3d':
-        let entradas3d = parseInt(prompt('ingrese cantidad entradas 3d'))
+        let entradas3d = parseInt(prompt('ingrese cantidad entradas 3d ($550)'))
         document.write('El precio total cine 3d es $'+(entradas3d * 550))
         break;
         case '2d':
-            let entradas2d = parseInt(prompt('ingrese cantidad entradas 2d'))
+            let entradas2d = parseInt(prompt('ingrese cantidad entradas 2d($330)'))
             document.write('el precio total cine 2d es $'+(entradas2d * 330))
             break;
             default:
